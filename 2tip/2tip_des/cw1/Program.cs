e58a4984023c1ae93b80c879daee03aef2af0c12
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-09-04 16:06:26 +0200

    hh

---
+++ 2tip/2tip_des/cw1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Hello, World!");
+Console.Write("Hello, World!");
+//typ nazwa
+int? liczba=null;
+ulong a = 12122344454455545455;
+Console.WriteLine(liczba);
+Console.WriteLine(4/3);
+try{
+    liczba = Convert.ToInt32("10.0"); //proba wykonania
+    Console.WriteLine(liczba);
+}catch(FormatException ex){
+    Console.WriteLine(ex.Message); //przechwycenie wyjatku
+}
