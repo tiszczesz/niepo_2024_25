6ba957de45f08fa2dbb4ba242ffd1a24a49629bd
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-09-09 08:58:21 +0200

    kk

---
+++ 2tip/2tip_des/cw2/Program.cs
@@ -0,0 +1,31 @@
+﻿void cw1()
+{
+    try
+    {
+        Console.Write("Podaj pierwsza liczbe: ");
+        double liczba1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Podaj druga liczbe: ");
+        double liczba2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"{liczba1} + {liczba2} = {liczba1 + liczba2}");
+        Console.WriteLine($"{liczba1} - {liczba2} = {liczba1 - liczba2}");
+        Console.WriteLine($"{liczba1} * {liczba2} = {liczba1 * liczba2}");
+        // if (liczba2 != 0)
+        // {
+        //     Console.WriteLine($"{liczba1} / {liczba2} = {liczba1 / liczba2}");
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"{liczba1} / {liczba2} = BRAK WYNIKU");
+        // }
+        Console.WriteLine($"{liczba1} / {liczba2} = " 
+                + (liczba2 != 0 ? liczba1 / liczba2 : "BRAK WYNIKU"));
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        Console.WriteLine("Błędny format danych");
+    }
+
+}
+
+cw1();
---
+++ 3tip/web/cw1_empty/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw1_empty.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
