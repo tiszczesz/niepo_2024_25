6847d54a2cebc62545e5d834c041621416c080a2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-09-09 10:57:57 +0200

    gg

--- 3tip/web/cw1_empty/Controllers/HomeController.cs
+++ 3tip/web/cw1_empty/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
+using cw1_empty.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw1_empty.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MoviesRepo _repo;
+        public HomeController(IConfiguration configuration){
+            _repo = new MoviesRepo(configuration);
+        }
         // GET: HomeController
         public ActionResult Index()
         {
-            return View();
+            var movies = _repo.GetMovies();
+            return View(movies);
         }
 
     }
