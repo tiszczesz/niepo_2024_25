14b17e341b1348aebd9ceb7ee1d1c1f68964cbe4
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-09-11 16:00:42 +0200

    hh

---
+++ 2tip/2tip_des/cw3/Game.cs
@@ -0,0 +1,4 @@
+//klasa Game
+// pola title, category, year, price
+// metody: show(), buy()-> kupiłeś gre o tytule title za cenę price
+// nadpisac metodę toString()
--- 2tip/2tip_des/cw3/Person.cs
+++ 2tip/2tip_des/cw3/Person.cs
@@ -1,15 +1,41 @@
-public class Person{
+public class Person
+{
     //pola zmienne opisujace dany obiekt
-    public string firstname;
-    public string lastname;
-    public int age;
-
-    //metody funkcje definiujace zachowanie obiektu
-    public void Hello(){
-        Console.WriteLine("Hello jestem " + firstname + " " + lastname);
+    private string? firstname;
+    public string? Firstname
+    {
+        get { return firstname.ToUpper(); }
+        set
+        {
+            if (value.Length < 3) firstname = "noname";
+            else firstname = value;
+        }
+    }
+    private string? lastname;
+    public string? Lastname
+    {
+        get { return lastname.ToUpper(); }
+        set
+        {
+            if (value.Length < 3) lastname = "noname";
+            else lastname = value;
+        }
+    }
+    private int age;
+    public int Age
+    {
+        get { return age; }
+        set { age = value > 0 ? value : -value; }
     }
 
-    public override string ToString(){
-        return firstname + " " + lastname + " wiek " + age ;
+    //metody funkcje definiujace zachowanie obiektu
+    public void Hello()
+    {
+        Console.WriteLine("Hello jestem " + Firstname + " " + Lastname);
+    }
+
+    public override string ToString()
+    {
+        return Firstname + " " + Lastname + " wiek " + Age;
     }
 }
