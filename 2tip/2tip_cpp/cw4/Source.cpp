6c3d03f4363203790e4ee3ac40f451a8f8224cb7
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-09-18 09:03:09 +0200

    gg

--- 2tip/2tip_cpp/cw4/Source.cpp
+++ 2tip/2tip_cpp/cw4/Source.cpp
@@ -4,5 +4,24 @@
 using namespace std;
 
 int main() {
+	int tab[] = { 1, 2, 3, 4, 5 };//definicja tablicy z inicjalizacj¹
+	int tab2[10]; //definicja tablicy 10-elementowej
+	for(int i = 0; i<5;i++)
+		cout << tab[i] << " ";
+	cout << endl;
+	for (int i = 0; i < 10; i++)
+		cout << tab2[i] << " ";
+	cout << endl;
+	//tab[5] = 6; //przypisanie wartoœci do elementu tablicy spoza zakresu
+
+	double liczby[10];//deklaracja tablicy 10-elementowej double
+	liczby[0] = 3.14; //przypisanie wartoœci do elementu tablicy
+	for (int i = 0; i < 10; i++)
+		cout << liczby[i] << " ";
+	cout << endl;
+	//deklaracja i inicjalizacja tablicy napisów
+	string napisy[] = { "Ala", "ma", "kota", "a", "kot" };
+	
 	return 0;
+
 }
