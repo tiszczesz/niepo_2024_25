546fcddbde626fe02f68bc76b186d087f8826fb6
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-09-27 08:45:41 +0200

    hh

--- 4tip/4ti_web/nodejs-typescript/cw4/dist/cw4.js
+++ 4tip/4ti_web/nodejs-typescript/cw4/dist/cw4.js
@@ -1,2 +1,37 @@
 import { people } from "./data.js";
 console.log(people);
+const root = document.querySelector("#root");
+if (root) {
+    generButton(root);
+}
+function generButton(elem) {
+    const button = document.createElement("button");
+    button.textContent = "Załaduj dane";
+    elem.appendChild(button);
+    button.addEventListener("click", () => {
+        generTable(elem);
+    });
+}
+function generTable(elem) {
+    const table = document.createElement("table");
+    table.className = "table table-striped";
+    const header = table.createTHead();
+    const row = header.insertRow(0);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    cell1.innerHTML = "<b>Imię</b>";
+    cell2.innerHTML = "<b>Nazwisko</b>";
+    cell3.innerHTML = "<b>Wiek</b>";
+    const body = table.createTBody();
+    people.forEach((person) => {
+        const row = body.insertRow();
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        cell1.textContent = person.firstname;
+        cell2.textContent = person.lastname;
+        cell3.textContent = person.age.toString();
+    });
+    elem.appendChild(table);
+}
--- 4tip/4ti_web/nodejs-typescript/cw4/src/cw4.ts
+++ 4tip/4ti_web/nodejs-typescript/cw4/src/cw4.ts
@@ -1,7 +1,42 @@
 import { people } from "./data.js";
 
 console.log(people);
+const root = document.querySelector<HTMLDivElement>("#root");
+
+if (root) {
+    generButton(root);
+}
 
 function generButton(elem:HTMLElement){
-
+    const button = document.createElement("button");
+    button.textContent = "Załaduj dane";
+    elem.appendChild(button);
+    button.addEventListener("click", () => {
+        generTable(elem);
+    });
+}
+function generTable(elem:HTMLElement):void{
+    const table = document.createElement("table");
+    table.className = "table table-striped";   
+    const header = table.createTHead();
+    const row = header.insertRow(0);
+   // row.innerHTML = "<th>Imię</th><th>Nazwisko</th><th>Wiek</th>";
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    cell1.innerHTML = "Imię";
+    cell2.innerHTML = "Nazwisko";
+    cell3.innerHTML = "Wiek";
+    const body = table.createTBody();
+    people.forEach((person) => {
+        const row = body.insertRow();
+      //  row.innerHTML = "<td>"+person.firstname+"</td><td>"+person.lastname+"</td><td>"+person.age+"</td>";
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        cell1.textContent = person.firstname;
+        cell2.textContent = person.lastname;
+        cell3.textContent = person.age.toString();
+    });
+    elem.appendChild(table);
 }
