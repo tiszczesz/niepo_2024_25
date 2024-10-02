489d328da6aecb300d4126f2aa8f7d7e590abfdf
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-10-02 10:11:47 +0200

    Update Source.cpp

--- 2tip/2tip_cpp/cw6/Source.cpp
+++ 2tip/2tip_cpp/cw6/Source.cpp
@@ -4,7 +4,12 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl;
+	int tab[50];
+
+	//napisaæ funkcjê, która wype³ni tablicê liczbami losowymi
+	FillTab(tab, 50);
+	//napisaæ funkcjê, która wypisze tablicê
+	ShowTab(tab, 50);
 	Show("To jest z funkcji: Hello World!");//uzycie funkcji z pliku functions.h
 	Hello();//uzycie funkcji z pliku functions.cpp
 }
