aec9a0478d2ec3560198f199cfdcd1656c4a71fa
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-10-03 16:54:42 +0200

    jjj

--- 2tip/2tip_des/cw9-wf/Form1.Designer.cs
+++ 2tip/2tip_des/cw9-wf/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace cw9_wf;
+﻿
+namespace cw9_wf;
 
 partial class Form1
 {
@@ -30,6 +31,9 @@ partial class Form1
     {
         label1 = new Label();
         button1 = new Button();
+        button2 = new Button();
+        pColor = new Panel();
+        label2 = new Label();
         SuspendLayout();
         // 
         // label1
@@ -37,7 +41,7 @@ partial class Form1
         label1.BackColor = Color.FromArgb(255, 255, 128);
         label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
         label1.ForeColor = Color.Purple;
-        label1.Location = new Point(343, 58);
+        label1.Location = new Point(346, 58);
         label1.Name = "label1";
         label1.Size = new Size(100, 100);
         label1.TabIndex = 0;
@@ -45,28 +49,62 @@ partial class Form1
         // 
         // button1
         // 
-        button1.Location = new Point(611, 64);
+        button1.Location = new Point(343, 12);
         button1.Name = "button1";
-        button1.Size = new Size(75, 23);
+        button1.Size = new Size(103, 43);
         button1.TabIndex = 1;
-        button1.Text = "button1";
+        button1.Text = "Pokaż/Ukryj";
         button1.UseVisualStyleBackColor = true;
         button1.Click += button1_Click;
+        // 
+        // button2
+        // 
+        button2.Location = new Point(49, 58);
+        button2.Name = "button2";
+        button2.Size = new Size(103, 43);
+        button2.TabIndex = 2;
+        button2.Text = "zmień kolor";
+        button2.UseVisualStyleBackColor = true;
+        button2.Click += button2_Click;
+        // 
+        // pColor
+        // 
+        pColor.Location = new Point(32, 113);
+        pColor.Name = "pColor";
+        pColor.Size = new Size(151, 223);
+        pColor.TabIndex = 3;
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(275, 91);
+        label2.Name = "label2";
+        label2.Size = new Size(0, 15);
+        label2.TabIndex = 4;
         // 
         // Form1
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(800, 450);
-        Controls.Add(button1);
+        Controls.Add(label2);
         Controls.Add(label1);
+        Controls.Add(button2);
+        Controls.Add(pColor);
+        Controls.Add(button1);
         Name = "Form1";
         Text = "Form1";
         ResumeLayout(false);
+        PerformLayout();
     }
+
+   
 
     #endregion
 
     private Label label1;
     private Button button1;
+    private Button button2;
+    private Panel pColor;
+    private Label label2;
 }
--- 2tip/2tip_des/cw9-wf/Form1.cs
+++ 2tip/2tip_des/cw9-wf/Form1.cs
@@ -11,4 +11,22 @@ public partial class Form1 : Form
     {
         label1.Visible = label1.Visible ? false : true;
     }
+
+    private void button2_Click(object sender, EventArgs e) {
+        var rnd = new Random();
+        var colors = new Color[] {
+            Color.Red, 
+            Color.Green,
+            Color.Blue, 
+            Color.Yellow, 
+            Color.Purple
+        };
+        if (pColor.BackColor != Color.Red) {
+            pColor.BackColor = Color.Red;
+        }
+        else {
+            pColor.BackColor = Color.Green;
+        }
+        
+    }
 }
