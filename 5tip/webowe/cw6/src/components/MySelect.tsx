aec321949852b05ea03494e714505bf250a0dd5b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-10-04 10:12:46 +0200

    mm

---
+++ 5tip/webowe/cw6/src/components/MySelect.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const MySelect = (props: Props) => {
+  return (
+    <div>MySelect</div>
+  )
+}
+
+export default MySelect
---
+++ 5tip/webowe/cw6/src/data.ts
@@ -0,0 +1,48 @@
+export interface BiolDefs {
+    def: string;
+    info: string;
+  }
+  
+ export const defs: BiolDefs[] = [
+    {
+      def: "Komórka",
+      info: "Podstawowa jednostka strukturalna i funkcjonalna organizmów żywych."
+    },
+    {
+      def: "DNA",
+      info: "Kwas deoksyrybonukleinowy, nośnik informacji genetycznej w komórkach."
+    },
+    {
+      def: "Fotosynteza",
+      info: "Proces, w którym rośliny, algi i niektóre bakterie przekształcają światło słoneczne w energię chemiczną."
+    },
+    {
+      def: "Ewolucja",
+      info: "Proces zmiany cech dziedzicznych w populacjach organizmów na przestrzeni pokoleń."
+    },
+    {
+      def: "Gen",
+      info: "Podstawowa jednostka dziedziczenia, która determinuje powstanie białka, zapisana w sekwencji nukleotydów."
+    },
+    {
+      def: "Mutacja",
+      info: "Zmiana w sekwencji DNA, która może prowadzić do zmiany cech organizmu."
+    },
+    {
+      def: "Ekosystem",
+      info: "Złożony system składający się z organizmów żywych i ich środowiska nieożywionego, które wzajemnie na siebie oddziałują."
+    },
+    {
+      def: "Biosfera",
+      info: "Strefa na Ziemi, w której istnieje życie, obejmująca litosferę, hydrosferę i atmosferę."
+    },
+    {
+      def: "Enzym",
+      info: "Białko, które katalizuje reakcje chemiczne w organizmach żywych."
+    },
+    {
+      def: "Homeostaza",
+      info: "Zdolność organizmu do utrzymania stabilnych warunków wewnętrznych pomimo zmian w środowisku zewnętrznym."
+    }
+  ];
+  
