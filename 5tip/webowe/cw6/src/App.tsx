58394251a9f5f10e7bbd08c0ec4aca373f21b81f
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-10-04 11:03:24 +0200

    jj

--- 5tip/webowe/cw6/src/App.tsx
+++ 5tip/webowe/cw6/src/App.tsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
-
+import { defs } from './data'
 import './App.css'
 import MySelect from './components/MySelect'
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='container'>
      <h1>Ćwiczenie 6</h1>
-     <MySelect />
+     <MySelect data={defs} />
     </div>
   )
 }
--- 5tip/webowe/cw6/src/components/MySelect.tsx
+++ 5tip/webowe/cw6/src/components/MySelect.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
 
-type Props = {}
+import { ChangeEvent, useState } from 'react'
+import { BiolDefs } from '../data'
+
+type Props = {
+    data: BiolDefs[]
+}
 
 const MySelect = (props: Props) => {
-  return (
-    <div>MySelect</div>
-  )
+    const [selected, setSelected] = useState<BiolDefs>({ def: "", info: "" })
+
+
+    function handleSelectChange(e: ChangeEvent<HTMLSelectElement>): void {
+        const current = props.data.find((elem) => elem.def === e.target.value)
+        if (current) setSelected(current)
+    }
+
+    return (
+        <section className='d-flex m-3 gap-3' >
+            <select value={selected.def} onChange={(e) => handleSelectChange(e)}>
+                {props.data.map((elem) => (
+                    <option key={elem.def}>{elem.def}</option>
+                ))}
+            </select>
+            <article style={
+                {
+                    border: "solid black 1px",
+                    minWidth: "300px",
+                    padding: "0.5rem",
+                }}>
+                {selected.info}
+            </article>
+        </section>
+    )
 }
 
 export default MySelect
+
