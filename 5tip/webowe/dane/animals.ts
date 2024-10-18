f6b822c37eb553bc7014d9f32027c9d57cd1667d
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-10-18 12:14:32 +0200

    k

---
+++ 5tip/webowe/dane/animals.ts
@@ -0,0 +1,8 @@
+export const Animals: string[] = [
+    "Lew", "Tygrys", "Słoń", "Żyrafa", "Zebra", "Kangur", "Panda", "Koala", 
+    "Lampart", "Gepard", "Nosorożec", "Hipopotam", "Krokodyl", "Aligator", 
+    "Szympans", "Goryl", "Orangutan", "Pawian", "Lemur", "Leniwiec", "Pancernik", 
+    "Mrówkojad", "Jeżozwierz", "Jeż", "Wydra", "Bóbr", "Szop", "Skunks", 
+    "Borsuk", "Rosomak", "Łoś", "Jeleń", "Bizon", "Bawół", "Antylopa", 
+    "Gazela", "Wielbłąd", "Lama", "Alpaka", "Jak"
+  ];
