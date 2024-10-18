9bda35c751da918b2279d0e12347754eee84d41e
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-10-18 16:37:06 +0200

    hh

--- 2tip/2tip_web/cw4-forms/Pages/Index.cshtml.cs
+++ 2tip/2tip_web/cw4-forms/Pages/Index.cshtml.cs
@@ -7,6 +7,19 @@ namespace cw4_forms.Pages
     {
         public void OnGet()
         {
+            var request = Request;
+            ViewData["method"] = request.Method;
+            ViewData["firstname"] = request.Query["firstname"];
+            ViewData["lastname"] = request.Query["lastname"];
+            ViewData["age"] = request.Query["age"];
+        }
+        public void OnPost()
+        {
+            var request = Request;
+            ViewData["method"] = request.Method;
+            ViewData["firstname"] = request.Form["firstname"];
+            ViewData["lastname"] = request.Form["lastname"];
+            ViewData["age"] = request.Form["age"];
         }
     }
 }
