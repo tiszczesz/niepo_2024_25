59c00c18166e9ffc4743f4fb73e667ad9b6a5a93
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-10-23 14:10:58 +0200

    44

---
+++ 4tip/4tip_des/cw3_grid/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace cw3_grid
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 4tip/4tip_des/cw3_grid/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cw3_grid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+            MessageBox.Show("Hello");
+        }
+
+        private void TbA_OnPreviewTextInput(object sender, TextCompositionEventArgs e) {
+            e.Handled = IsTextAllowed(e.Text);
+        }
+        private bool IsTextAllowed(string text)
+        {
+            Regex regex = new Regex("[^0-9]+"); // Tylko cyfry
+            return regex.IsMatch(text);
+        }
+    }
+}
