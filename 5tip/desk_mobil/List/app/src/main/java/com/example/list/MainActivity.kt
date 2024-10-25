60aab07d35840278861333127aea617d44eca3af
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-10-25 12:41:42 +0200

    kkk

--- 5tip/desk_mobil/List/app/src/main/java/com/example/list/MainActivity.kt
+++ 5tip/desk_mobil/List/app/src/main/java/com/example/list/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.list
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
@@ -24,5 +25,39 @@ class MainActivity : AppCompatActivity() {
         val addNew = findViewById<Button>(R.id.add)
         val task = findViewById<EditText>(R.id.task)
         val spinner = findViewById<Spinner>(R.id.spinner)
+        //zadania wstępne
+        val todos = arrayListOf<Todo>(
+            Todo("Zrobić zakupy", MyPriority.ALERT),
+            Todo("Obejrzeć film", MyPriority.MEDIUM),
+            Todo("Zrobić prezentację", MyPriority.HIGH),
+            Todo("Posprzątać pokój", MyPriority.LOW)
+        )
+        val priority = arrayListOf<MyPriority>(
+            MyPriority.ALERT,
+            MyPriority.HIGH,
+            MyPriority.MEDIUM,
+            MyPriority.LOW
+        )
+        //jak datasource dla spinnera
+        val adapterSpinner = ArrayAdapter<MyPriority>(
+            this,
+            android.R.layout.simple_spinner_item, priority)
+        spinner.adapter = adapterSpinner //podpięcie adaptera do spinnera
+
+        //dla listview
+        val adapterTodo = ArrayAdapter<Todo>(
+            this,
+            android.R.layout.simple_list_item_1, todos)
+        list.adapter = adapterTodo //podpięcie adaptera do listview
+ //dodawanie do listy
+        addNew.setOnClickListener {
+            //pobranie wartości z pól
+            val newTask = task.text.toString().trim()
+            val newPriority = spinner.selectedItem as MyPriority
+            //dodanie do listy
+            todos.add(Todo(newTask, newPriority))
+            //odświeżenie adaptera
+            adapterTodo.notifyDataSetChanged()
+        }
     }
 }
