73280876474ec2492da243ec99c3637eb0d39431
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-10-30 16:05:48 +0100

    hh

---
+++ 2tip/2tip_web/cw5/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cw5.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
