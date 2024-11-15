74a3036077f115ed47ff5306d0d3f11baf44dba6
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-11-15 16:04:53 +0100

    hh

--- 3tip/desk/cw3_sqlite/Form1.cs
+++ 3tip/desk/cw3_sqlite/Form1.cs
@@ -1,10 +1,20 @@
+using cw3_sqlite.Models;
+
 namespace cw3_sqlite
 {
     public partial class Form1 : Form
     {
+        private AnimalsRepo _repo;
+
         public Form1()
         {
             InitializeComponent();
+            _repo = new AnimalsRepo();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            dataGridView1.DataSource = _repo.GetAnimals();
+            dataGridView1.Columns["id"].Visible = false;
         }
     }
 }
---
+++ 3tip/desk/cw3_sqlite/Models/AnimalsRepo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace cw3_sqlite.Models
+{
+    public class AnimalsRepo
+    {
+        private string connString = "Data Source=animals.db";
+        public AnimalsRepo() { }
+
+        public List<Animal> GetAnimals() {
+            var result = new List<Animal>();
+            using SqliteConnection conn = new SqliteConnection(connString);
+            SqliteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Animal";
+            conn.Open();
+            using SqliteDataReader reader = cmd.ExecuteReader();
+            while (reader.Read()) {
+                result.Add(new Animal
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Species = reader.GetString(2),
+                    BitrhDay = DateOnly
+                        .FromDateTime( reader.GetDateTime(3))
+                });
+            }
+
+            return result;
+        }
+    }
+}
