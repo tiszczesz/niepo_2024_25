0042a44706c6ca49178bab8f6a7346707ad09666
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-11-15 15:38:40 +0100

    kk

---
+++ 3tip/desk/cw3_sqlite/SQLite.sql
@@ -0,0 +1,17 @@
+-- SQLite
+CREATE TABLE animal(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    species TEXT,
+    birthdate DATE
+);
+INSERT INTO animal (id, name, species, birthdate) VALUES (1, 'Bella', 'Dog', '2015-06-01');
+INSERT INTO animal (id, name, species, birthdate) VALUES (2, 'Max', 'Cat', '2017-08-15');
+INSERT INTO animal (id, name, species, birthdate) VALUES (3, 'Charlie', 'Rabbit', '2018-03-22');
+INSERT INTO animal (id, name, species, birthdate) VALUES (4, 'Lucy', 'Dog', '2016-11-30');
+INSERT INTO animal (id, name, species, birthdate) VALUES (5, 'Daisy', 'Cat', '2019-01-10');
+INSERT INTO animal (id, name, species, birthdate) VALUES (6, 'Milo', 'Hamster', '2020-05-05');
+INSERT INTO animal (id, name, species, birthdate) VALUES (7, 'Luna', 'Dog', '2014-09-17');
+INSERT INTO animal (id, name, species, birthdate) VALUES (8, 'Oliver', 'Cat', '2018-12-25');
+INSERT INTO animal (id, name, species, birthdate) VALUES (9, 'Simba', 'Lion', '2013-07-14');
+INSERT INTO animal (id, name, species, birthdate) VALUES (10, 'Nala', 'Lion', '2013-07-14');
