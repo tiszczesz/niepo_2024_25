4d324b94729828cd8b25c8659474479039264920
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-11-15 10:12:54 +0100

    kk

---
+++ 5tip/webowe/cw11-courses/src/App.tsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='container'>
+     <h1>Kursy dla programistów</h1>
+    </div>
+  )
+}
+
+export default App
