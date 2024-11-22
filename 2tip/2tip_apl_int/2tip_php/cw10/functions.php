25090fee5e39de25a4de4b5b4dd1a7419faf074b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-11-22 09:17:14 +0100

    kk

---
+++ 2tip/2tip_apl_int/2tip_php/cw10/functions.php
@@ -0,0 +1,21 @@
+<?php
+function getDrinks():array { //do select
+    return [
+        'Coca Cola' => 5.99,
+        'Pepsi' => 4.99,
+        'Sprite' => 3.99,
+        'Fanta' => 2.99,
+        'Mirinda' => 1.99
+    ];    
+}
+function getFood():array { //do checkboxes
+    return [
+        'Pizza' => 15.99,
+        'Kebab' => 14.99,
+        'Burger' => 13.99,
+        'HotDog' => 12.99,
+        'Frytki' => 11.99,
+        'Kurczak' => 10.99,
+        'Kotlet' => 9.99
+    ];
+}
