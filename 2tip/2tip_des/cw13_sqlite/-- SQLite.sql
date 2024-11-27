c17806f79d702cf99f4b2cb17d15b2cd73a0c38d
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-11-27 15:03:18 +0100

    kk

---
+++ 2tip/2tip_des/cw13_sqlite/-- SQLite.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE products(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    price REAL,
+    description TEXT
+)
+
+INSERT INTO products (name, price, description) VALUES ('Jabłka', 23.5, 'pyszne czerwone');
+INSERT INTO products (name, price, description) VALUES ('Gruszki', 33.5, 'słodziutkie');
+INSERT INTO products (name, price, description) VALUES ('Pomarańcze', 43.5, 'pomarańczowe');
+INSERT INTO products (name, price, description) VALUES ('Mandarynki', 53.5, 'mandarynkowe');
+INSERT INTO products (name, price, description) VALUES ('Banany', 63.5, 'bananowe');
