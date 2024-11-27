37d15959e35953f3a138dce47d6f4c73a51f5773
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-11-27 11:05:38 +0100

    ll

--- 2tip/2tip_cpp/cw16/Source.cpp
+++ 2tip/2tip_cpp/cw16/Source.cpp
@@ -17,13 +17,24 @@ void reference(int& a, int& b) {
 	cout << "adresy w funkcji: a: " << &a << " b: " << &b << endl;
 }
 
+void pointers(int* pa,int* pb) {
+	//int fake = 0;
+	//pa = &fake;
+	int temp = *pa;
+	*pa = *pb;
+	*pb = temp;
+	cout << "wewnatrz funkcji: a: " << *pa << " b: " << *pb << endl;
+	cout << "adresy w funkcji: a: " << pa << " b: " << pb << endl;
+}
+
 int main() {
 	int a = 5;
 	int b = 10;
 	cout << "adresy w main: a: " << &a << " b: " << &b << endl;
 	cout << "przed funkcja: a: " << a << " b: " << b << endl;
-	copy(a, b);
+	//copy(a, b);
 	//reference(a, b);
+	pointers(&a, &b);
 	cout << "po funkcji: a: " << a << " b: " << b << endl;
 	return 0;
 }
