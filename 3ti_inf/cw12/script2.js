c8f9b966d4f8800bd88c29b1c7413d2731fc8694
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-11-28 17:36:38 +0100

    hh

--- 3ti_inf/cw12/script2.js
+++ 3ti_inf/cw12/script2.js
@@ -2,7 +2,7 @@ const inputs = document.querySelectorAll("li input");
 for(let input of inputs) {
     input.onclick = function(event) {
       const span = event.target.previousElementSibling;
-      span.style.textDecoration = "line-through";
+      span.style.textDecoration = event.target.checked ? "line-through": "none";
     }
     
 }
