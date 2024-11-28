888142b380c9e1f0838a3cd7b18b072ccc24f358
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-11-28 16:33:54 +0100

    kk

---
+++ 2tip/2tip_des/cw14_sqlite/Form1.cs
@@ -0,0 +1,10 @@
+namespace cw14_sqlite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 2tip/2tip_des/cw14_sqlite/SQLite.sql
@@ -0,0 +1,14 @@
+-- SQLite
+CREATE TABLE cars (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    price float,
+    brand TEXT,
+    prodYear INTEGER
+);
+INSERT INTO cars (name, price, brand, prodYear) VALUES ('Audi', 52642, 'Audi', 2017);
+INSERT INTO cars (name, price, brand, prodYear) VALUES ('Mercedes', 57127, 'Mercedes', 2018);
+INSERT INTO cars (name, price, brand, prodYear) VALUES ('Skoda', 9000, 'Skoda', 2017);
+INSERT INTO cars (name, price, brand, prodYear) VALUES ('Volvo', 29000, 'Volvo', 2018);
+INSERT INTO cars (name, price, brand, prodYear) VALUES ('Bentley', 350000, 'Bentley', 2018);
+INSERT INTO cars (name, price, brand, prodYear) VALUES ('Citroen', 21000, 'Citroen', 2018);
