3b88b76ef1f1c2a3ce0eaba9830b7fc6a698fc4b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-12-06 09:40:39 +0100

    kk

--- 2tip/2tip_apl_int/2tip_php/cw11/form.php
+++ 2tip/2tip_apl_int/2tip_php/cw11/form.php
@@ -1,6 +1,12 @@
 <?php
+if (!isset($_GET['page']) || $_GET['page'] !== 'form') {
+    header('Location: layout.php?page=home');
+    exit();
+}
+
 
 $html = <<<html
+<h1>Formularz 2024</h1>
 <h1>
     <form method="post">
         <input type="text" placeholder="podaj imię" name="name">
---
+++ 2tip/2tip_apl_int/2tip_php/cw12/cw12.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pliki</title>
+</head>
+
+<body>
+    <h1>Operacje na plikach</h1>
+    <?php
+    const FILE_NAME = "info.txt";
+    $file = fopen(FILE_NAME, "r");
+    if ($file) {
+        while (($line = fgets($file))) {
+            echo $line . "<br>";
+        }
+        fclose($file);
+    } else {
+        echo "Nie udało się otworzyć pliku";
+    }
+    $words = ["Ala", "ma", "kota"];
+    $file = fopen("output.txt", "a");
+    if ($file) {
+        foreach ($words as $word) {
+            fwrite($file, $word . "-");
+        }
+        fclose($file);
+    } else {
+        echo "Nie udało się otworzyć pliku";
+    }
+    ?>
+</body>
+
+</html>
