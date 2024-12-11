a877e27b48491d13efa6df415a7f493739232c07
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-12-11 11:02:01 +0100

    kk

--- 2tip/2tip_cpp/cw17/Source.cpp
+++ 2tip/2tip_cpp/cw17/Source.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
+
+#include <filesystem>
 using namespace std;
-//napisac funkcje ktora prosi o podanie imienia, nazwiska i wieku
-//ale uzywamy tylko scanf printf c-stringi
-int main()
-{
+namespace fs = std::filesystem;
+
+void AddCString(const char* str1,const char* str2) {
+	//napisz funkcje zwracajaca rozmiar c-stringa
+	//po³¹cz str1 i str2 i wypisz wynik ala\0
+	//cout << str1 << " " << str2 << endl;
+}
+void cw1() {
 	const char* info = "Hello, World!";
 	//char** war =  {"dddd","gggg"} ;
 	const char* war[] = { "dddd", "gggg" };
@@ -23,5 +29,29 @@ int main()
 	string result2 = n1 + " " + n2 + " " + n3;
 	//string result = info + " " + info2 ;
 	cout << info2 << endl;
+}
+int getSize(const char* str) {
+	int i = 0;
+	while (str[i] != '\0') {
+		i++;
+	}
+	return i;
+}
+void ScanDir(const std::string path) {	
+	for (const auto& entry : fs::directory_iterator(path))
+		std::cout << entry.path() << std::endl;
+
+}
+//napisac funkcje ktora prosi o podanie imienia, nazwiska i wieku
+//ale uzywamy tylko scanf printf c-stringi
+int main()
+{
+	//cw1();
+	char napis1[] = "ala";
+	char napis2[] = "ma";
+	cout << getSize(napis1) << endl;
+	AddCString(napis1, napis2);//polacz napis1 i napis2 i wypisz wynik
+	fs::path currentPath = fs::current_path();
+	ScanDir(currentPath.string());
 	return 0;
 }
