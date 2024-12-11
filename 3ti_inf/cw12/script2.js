59b976a8241728a505d52b504831827666693390
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-12-11 14:03:37 +0100

    kk

--- 3ti_inf/cw12/script2.js
+++ 3ti_inf/cw12/script2.js
@@ -1,4 +1,6 @@
 const inputs = document.querySelectorAll("li input");
+console.log(inputs);
+
 for(let input of inputs) {
     input.onclick = function(event) {
       const span = event.target.previousElementSibling;
