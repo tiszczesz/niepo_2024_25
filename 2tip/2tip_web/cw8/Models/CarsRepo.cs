25626e3ab873daf259b7eb918ddd9e7c4d90e1bb
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-12-13 16:29:31 +0100

    kk

--- 2tip/2tip_web/cw8/Models/CarsRepo.cs
+++ 2tip/2tip_web/cw8/Models/CarsRepo.cs
@@ -4,18 +4,18 @@ namespace cw8.Models;
 
 public class CarsRepo
 {
-    public static List<Car> GetCars(){
-        return new List<Car>{
-            new Car{Id=1, Model="A4", Brand="Audi", Year=2018, Color="Black"},
-            new Car{Id=2, Model="A5", Brand="Audi", Year=2019, Color="White"},
-            new Car{Id=3, Model="A6", Brand="Audi", Year=2020, Color="Red"},
-            new Car{Id=4, Model="A7", Brand="Audi", Year=2021, Color="Blue"}
+    public static List<MyCar> GetCars(){
+        return new List<MyCar>{
+            new MyCar{Id=1, Model="A4", Brand="Audi", Year=2018, Color="Black"},
+            new MyCar{Id=2, Model="A5", Brand="Audi", Year=2019, Color="White"},
+            new MyCar{Id=3, Model="A6", Brand="Audi", Year=2020, Color="Red"},
+            new MyCar{Id=4, Model="A7", Brand="Audi", Year=2021, Color="Blue"}
         };
     }
 
 }
 
-public class Car
+public class MyCar
 {
     public int Id { get; set; }
     public string Model { get; set; }
