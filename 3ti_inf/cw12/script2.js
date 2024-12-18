591f174e6f3ff79a40b0d13086f813fd03396fc6
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-12-18 14:19:33 +0100

    hh

--- 3ti_inf/cw12/script2.js
+++ 3ti_inf/cw12/script2.js
@@ -1,10 +1,25 @@
-const inputs = document.querySelectorAll("li input");
-console.log(inputs);
+function addIsDone() {
+  const inputs = document.querySelectorAll("li input");
+  console.log(inputs);
 
-for(let input of inputs) {
-    input.onclick = function(event) {
+  for (let input of inputs) {
+    input.checked = false;
+    input.onclick = function (event) {
       const span = event.target.previousElementSibling;
-      span.style.textDecoration = event.target.checked ? "line-through": "none";
+      span.style.textDecoration = event.target.checked ? "line-through" : "none";
     }
-    
+
+  }
 }
+addIsDone();
+document.querySelector("#btnAdd").onclick = function () {
+  const text = document.querySelector("#task").value;
+  if (text.trim() === "") return;
+  const li = document.createElement("li");
+  li.innerHTML = `<span>${text}</span> <input type="checkbox">`;
+  // li.innerHTML = '<span>' + text + '</span> <input type="checkbox">';
+  const ol = document.querySelector("ol");
+  ol.appendChild(li);
+  addIsDone();
+  document.querySelector("#task").value = "";
+};
