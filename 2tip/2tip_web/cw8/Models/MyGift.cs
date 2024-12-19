6d640ec2e5481a71a29e9ed9596926f2a3a12a41
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-12-19 11:06:44 +0100

    kk

---
+++ 2tip/2tip_web/cw8/Models/MyGift.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace cw8.Models;
+
+public class MyGift
+{
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Owner { get; set; }
+}
