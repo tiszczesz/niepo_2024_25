aa0fa6e3dace41a69d5cb2ee5392c4ca120406d8
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-01-08 10:17:41 +0100

    ll

--- 2tip/2tip_cpp/cw18_ob1/BooksRepo.h
+++ 2tip/2tip_cpp/cw18_ob1/BooksRepo.h
@@ -10,6 +10,8 @@ public:
 		books.push_back(Book("Wladca Pierscieni", "J.R.R. Tolkien", 1954, 1000));
 		books.push_back(Book("Harry Potter", "J.K. Rowling", 1997, 500));
 		books.push_back(Book("Zakopane noca", "Roman Boman", 2004, 200));
+		books.push_back(Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 1911, 400));
+		return books;
 	}
 	
 };
