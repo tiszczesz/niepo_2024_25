53e50f684afd78bded8655fea796722e07121021
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-02-05 13:44:14 +0100

    kk

---
+++ 4tip/mobileApp/cw2/cw2/src/cw1.kt
@@ -0,0 +1,18 @@
+fun main(){
+ //cc1()
+}
+fun cc1(){
+    val a = 12 //niemutowalna
+    var c:Int
+    var b:Int = 16 //mutowalna
+    println("a = $a, b = $b")
+  //  a = 34 //nie można zmienić wartości
+    b = 45 //można zmienić wartość
+    c = 23
+    println("a = $a, b = $b, c = $c")
+}
+fun cc2(){
+    val firstname = "Jan"
+    val elem = 'a'
+    print(firstname.uppercase())
+}
