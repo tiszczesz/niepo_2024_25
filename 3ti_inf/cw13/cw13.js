2747be31005330cfbd6e607e41637f5737e92978
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-02-12 14:19:07 +0100

    kk

--- 3ti_inf/cw13/cw13.js
+++ 3ti_inf/cw13/cw13.js
@@ -5,10 +5,22 @@ form.onsubmit = function (event) {
     //bez walidacji
     const rows = parseInt(document.querySelector('#rows').value);
     const cols = parseInt(document.querySelector('#cols').value);
-    console.log(rows, cols);
+    //console.log(rows, cols);
     const table = createTable(rows, cols);
     document.querySelector('.result').innerHTML = table;
+    console.log('submit', event);
+
 };
+document.querySelector("#reset").onclick = function (event) {
+    //alert('reset');
+    event.preventDefault();
+    document.querySelector('#rows').value = 20;
+    document.querySelector('#cols').value = 20;
+    //ustawianie inputów na value 20 oraz czyszcze element .result
+    document.querySelector('.result').innerHTML = '';
+    console.log(document.querySelector('.result').innerHTML, event);
+
+}
 
 function createTable(rows, cols) {
     let html = '<table>';
@@ -19,10 +31,18 @@ function createTable(rows, cols) {
         //po kolumnach
         for (let j = 0; j < cols; j++) {
             number++;
-            html += `<td>${number}</td>`;
+            let isRed = isPrime(number) ? "class='red'" : '';
+            html += `<td ${isRed}>${number}</td>`;
         }
         html += '</tr>';
     }
 
     return html + '</table>';
+};
+function isPrime(number) {
+    if (number < 2) return false;
+    for (let i = 2; i * i <= number; i++) {
+        if (number % i === 0) return false;
+    }
+    return true;
 };
