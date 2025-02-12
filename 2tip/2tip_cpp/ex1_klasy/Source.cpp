2cf272a71c294098044b03d1b88f5cee38ce2527
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-02-12 08:15:53 +0100

    kk

---
+++ 2tip/2tip_cpp/ex1_klasy/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	
+	return 0;
+}
+
+//Cwiczenie napisz klase opisujaca po³oznie punktu na plaszczyznie (x,y)
+//oraz metody do obliczania odleglosci miedzy dwoma oraz od poczatku ukladu wspolrzednych
+//Napisz program ktory wczytuje wspolrzedne dwoch punktow i wypisuje odleglosc miedzy nimi
+//przeci¹¿ operatora << aby wypisywa³ punkt w postaci (x,y)
+//Point2D.h
