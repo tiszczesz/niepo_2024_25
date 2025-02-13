81cccfa0f51e7bf398940dd968f030ceb6299777
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-02-13 08:13:08 +0100

    kk

---
+++ temp_ex/tt1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(fun1().Name);
+dynamic fun1(){
+    return new{
+        Name = "John",
+        Age = 25
+    };
+}
