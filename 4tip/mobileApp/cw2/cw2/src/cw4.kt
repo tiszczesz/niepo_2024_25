245dad7c3b3d49450c71c82b2428c704b4b508be
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-02-19 13:26:16 +0100

    kk

---
+++ 4tip/mobileApp/cw2/cw2/src/cw4.kt
@@ -0,0 +1,35 @@
+import kotlin.random.Random
+
+fun main() {
+   // c41()
+    c42()
+}
+
+fun c41() {
+    //lista niemutowalna
+    val list = listOf<Int>(1, 2, 3, 4, 5)
+    println(list)
+    //list.add(7)
+    //list.remove(3)
+    val newList = list.subList(1, 3)
+    println(newList)
+    val list2 = List<Int>(20) { it -> Random.nextInt(1, 50) }
+    println(list2)
+    val list3 = list2.filter { it -> it % 2 == 0 }
+    println(list2.last { it -> it % 2 == 0 })
+    println(list3)
+    println("suma: ${list2.sum()}")
+    println("suma nieparzystych: ${ list2.filter { it -> it % 2 != 0 }.sum() }")
+    println("suma parzystych: ${ list2.filter { it -> it % 2 == 0 }.sum() }")
+}
+fun c42(){
+    //listy mutowalne mozna modyfikowac dodawac usuwac elementy
+    val list = mutableListOf<Int>(1, 2, 3, 4, 5)
+    println(list)
+    list.add(7)
+    println(list)
+    list.remove(3)
+    println(list)
+    list.removeAt(2)
+    println(list)
+}
