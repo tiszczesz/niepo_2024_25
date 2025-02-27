617a4e3bcfc58247e194eb577917aa2a4a2555fd
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-02-27 16:12:54 +0100

    hh

---
+++ 2tip/2tip_des/cw16_sqlite/Models/ProductsRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw16_sqlite.Models
+{
+    public class ProductsRepo
+    {
+
+    }
+}
