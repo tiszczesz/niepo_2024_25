29e6f5e70362c26ce039dac3ca6c59d54df18c3b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-03-03 08:29:17 +0100

    kk

---
+++ 3tip/web/ark3/ark3_solution/Controllers/OtherController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ark3_solution.Controllers
+{
+    public class OtherController : Controller
+    {
+        // GET: OtherController
+        public ActionResult NoticesList()
+        {
+            return View();
+        }
+
+    }
+}
