4ad568350aaf28d1dc52185a9bd1f554bc40de47
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-03-06 11:35:01 +0100

    kk

---
+++ 2tip/2tip_web/cw9_mysql/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw9_mysql.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: UsersController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
