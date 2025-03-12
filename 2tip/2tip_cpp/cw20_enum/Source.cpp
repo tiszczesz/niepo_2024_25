ab7d83699c4543b87444c7bfdca8e479c3650b59
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-03-12 11:13:16 +0100

    hh

---
+++ 2tip/2tip_cpp/cw20_enum/Source.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "User.h"
+//uzycie zmiennej do przechowywania roli uzytkownika
+using namespace std;
+string GetUserInfo(const User& u);
+int main() {
+	User u1;
+	User u2("gienek", "gienek", Role(2));
+	cout << GetUserInfo(u2);
+	return 0;
+}
+string GetUserInfo(const User& u) {
+	return "Info: login: " + u.GetLogin() + " haslo: "
+		+ u.GetPassword() + " rola: " + u.GetRoleName();
+}
---
+++ 2tip/2tip_cpp/cw20_enum/User.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+#include <string>
+enum class Role2;
+enum Role;//deklaracja stara wersja
+class User
+{
+private:
+	std::string login;
+	std::string password;
+	Role role;
+	Role2 role2;
+public:
+	User() {
+		login = "noname";
+		password = "noname";
+		role = Role(5);
+	}
+	User(std::string login,std::string password,Role role = Role(5)) {
+		this->login = login;
+		this->password = password;
+		this->role = role;
+	}
+	std::string GetLogin() const {
+		return this->login;
+	}
+	std::string GetPassword() const {
+		return this->password;
+	}
+	Role GetRole() const {
+		return this->role;
+	}
+	Role2 GetRole2() const {
+		return this->role2;
+	}
+	std::string GetRoleName() const {
+		switch (this->role) {
+		case 1:
+			return "ADMIN";
+		case 2:
+			return "USER";
+		case 3:
+			return "EDITOR";
+		case 5:
+			return "VIEWER";
+		default:
+			return "UNKNOWN";
+		}
+	}
+	std::string GetRoleName2() const {
+		switch (this->role2) {
+		case Role2::ADMIN:
+			return "ADMIN";
+		case Role2::USER:
+			return "USER";
+		case Role2::EDITOR:
+			return "EDITOR";
+		case Role2::VIEWER:
+			return "VIEWER";
+		default:
+			return "UNKNOWN";
+		}
+
+};
+
+enum Role { //definicja
+	ADMIN=1,
+	USER,
+	EDITOR,
+	VIEWER = 5
+};
+enum class Role2 { //definicja
+	ADMIN = 1,
+	USER,
+	EDITOR,
+	VIEWER = 5
+};
+
+
