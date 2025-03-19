1ceb9b47b60c450b8f9a0624bda330828cefab5c
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-03-19 09:27:28 +0100

    kkk

--- 2tip/2tip_cpp/cw20_enum/Source.cpp
+++ 2tip/2tip_cpp/cw20_enum/Source.cpp
@@ -2,10 +2,14 @@
 #include <string>
 #include <cstdlib>
 #include "User.h"
+#include "Worker.h"
 //uzycie zmiennej do przechowywania roli uzytkownika
 using namespace std;
 string GetUserInfo(const User& u);
 int main() {
+	Worker w1;
+	Worker w2("Roman", Worksite::Remote);
+	cout << w1 << w2;
 	User u1;
 	User u2("gienek", "gienek", Role(2));
 	cout << GetUserInfo(u2);
---
+++ 2tip/2tip_cpp/cw20_enum/Worker.h
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+enum class Worksite {
+    Office,
+    Remote,
+    Onsite
+};
+class Worker {
+public:
+	Worker()
+		: name("noname"), worksite(Worksite::Office) {
+	}
+
+    Worker(const std::string& name, Worksite worksite)
+        : name(name), worksite(worksite) {
+    }
+
+    std::string displayWorksite() const {
+        std::string info = "Worker " + name + " works ";
+        switch (worksite) {
+        case Worksite::Office:
+            return info +"in the office.\n" ;
+            
+        case Worksite::Remote:
+            return info+ "remotely.\n" ;           
+        case Worksite::Onsite:
+            return info+ "onsite.\n";
+		default:
+            return info + "Unknown worksite.\n";
+        }
+    }
+	friend std::ostream& operator<<(std::ostream& os, const Worker& w) {
+		os << w.displayWorksite();
+		return os;
+	}
+
+private:
+    std::string name;
+    Worksite worksite;
+};
+
