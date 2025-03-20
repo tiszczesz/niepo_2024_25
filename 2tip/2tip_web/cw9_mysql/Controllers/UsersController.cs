2cb7c6e2dde16fa200759d62a26afd87a731fe40
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-03-20 11:24:00 +0100

    uu

--- 2tip/2tip_web/cw9_mysql/Controllers/UsersController.cs
+++ 2tip/2tip_web/cw9_mysql/Controllers/UsersController.cs
@@ -24,6 +24,7 @@ namespace cw9_mysql.Controllers
                 return RedirectToAction("List");
             }
             return View(user);
-
+        }
+        
     }
 }
--- 2tip/2tip_web/cw9_mysql/Models/UsersRepo.cs
+++ 2tip/2tip_web/cw9_mysql/Models/UsersRepo.cs
@@ -60,4 +60,29 @@ public class UsersRepo
         return users;
     }
 
+    internal User? GetUserById(int id)
+    {
+        using var conn = new MySqlConnection(_connectionString);
+        using var cmd = conn.CreateCommand();
+        cmd.CommandText = "SELECT u.id as id,u.firstname as firstname,u.lastname as lastname,u.age as age, r.name as name " +
+        " FROM users as u INNER JOIN roles as r ON u.role_id = r.id " + "WHERE u.id = @id";
+        cmd.Parameters.AddWithValue("@id", id);
+        conn.Open();
+        User? user = null;
+        using var reader = cmd.ExecuteReader();
+        if (reader.HasRows)
+        {
+            reader.Read();
+            user = new User
+            {
+                Id = reader.GetInt32("id"),
+                Firstname = reader.GetString("firstname"),
+                Lastname = reader.GetString("lastname"),
+                Age = reader.GetInt32("age"),
+                RoleName = reader.GetString("name")
+            };
+        }
+        conn.Close();
+        return user;
+    }
 }
