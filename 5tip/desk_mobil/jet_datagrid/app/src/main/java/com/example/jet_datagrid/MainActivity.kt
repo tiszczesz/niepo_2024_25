c1e6b50ecc289bf30a35999ae8216b8df30269f4
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-03-24 09:06:51 +0100

    kk

--- 5tip/desk_mobil/jet_datagrid/app/src/main/java/com/example/jet_datagrid/MainActivity.kt
+++ 5tip/desk_mobil/jet_datagrid/app/src/main/java/com/example/jet_datagrid/MainActivity.kt
@@ -6,19 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
+
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import com.example.jet_datagrid.ui.theme.Jet_datagridTheme
 
@@ -28,17 +32,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Jet_datagridTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-                DataGrid()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MyWindow(modifier = Modifier.padding(innerPadding))
+                }
+               // DataGrid()
             }
         }
     }
 }
+//@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyWindow(modifier: Modifier) {
+    val showAlertDialog = remember { mutableStateOf(false) }
+    Column(modifier = Modifier.padding(50.dp)) {
+        Button(modifier = Modifier.fillMaxWidth(), onClick = { showAlertDialog.value = true }) {
+            Text("Show Dialog")
+        }
+    }
+    if(showAlertDialog.value) {
+       AlertDialog(onDismissRequest = {showAlertDialog.value=false}, confirmButton = {},
+           title = {Text("sddssdssfs")})
+    }
+
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -48,29 +65,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Jet_datagridTheme {
-        Greeting("Android")
-    }
-}
-@Preview
-@Composable
-fun DataGrid() {
-    val items = (0..20).toList()
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        modifier = Modifier.fillMaxSize()
-    ) {
-        items(items.size){index->
-            Box(contentAlignment = Alignment.Center,
-
-                modifier = Modifier.padding(8.dp).aspectRatio(1f)
-                    .border(2.dp, Color.Black)
-                    .background(Color.Green)) {
-                Text(text = "Item $index")
-            }
-        }
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    Jet_datagridTheme {
+//        Greeting("Android")
+//    }
+//}
+//@Preview
+//@Composable
+//fun DataGrid() {
+//    val items = (0..20).toList()
+//    LazyVerticalGrid(
+//        columns = GridCells.Fixed(2),
+//        modifier = Modifier.fillMaxSize()
+//    ) {
+//        items(items.size){index->
+//            Box(contentAlignment = Alignment.Center,
+//
+//                modifier = Modifier.padding(8.dp).aspectRatio(1f)
+//                    .border(2.dp, Color.Black)
+//                    .background(Color.Green)) {
+//                Text(text = "Item $index")
+//            }
+//        }
+//    }
+//}
