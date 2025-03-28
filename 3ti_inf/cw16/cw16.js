9007c69f79ac4b3457b9571fa19242afa6fa5a57
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-03-28 11:35:15 +0100

    kk

--- 3ti_inf/cw16/cw16.js
+++ 3ti_inf/cw16/cw16.js
@@ -1,5 +1,10 @@
 document.querySelector('#start').onclick = function (e) {
     document.querySelector('.scene').innerHTML = generTab(40, 40);
+    const tds = document.querySelectorAll('.table td');
+    console.log(tds);
+    for(td of tds){
+        
+    }
 }
 
 function generTab(rows, cols) {
