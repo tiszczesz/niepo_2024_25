a6b94935fdf054265f83f1a30b44b428c2437c96
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-04-09 15:15:02 +0200

    kk

---
+++ 2tip/2tip_web/cw10_cars/Models/Car.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace cw10_cars.Models;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string? Brand { get; set; }
+    public string? Model { get; set; }
+    public int? Year { get; set; }
+    public string? Color { get; set; }
+    public string? Condition { get; set; }
+
+}
