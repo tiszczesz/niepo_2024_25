8b3342eeecf159e3ef19b74bc55445dbfb755c3a
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-04-10 10:37:09 +0200

    jj

--- 4tip/mobileApp/cw1/app/src/main/java/com/example/cw1/MainActivity.kt
+++ 4tip/mobileApp/cw1/app/src/main/java/com/example/cw1/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.cw1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btn1 = findViewById<Button>(R.id.btn1)
+        btn1.setOnClickListener {
+            Toast.makeText(this,"Przycisk zadziałał",Toast.LENGTH_LONG).show()
         }
     }
 }
