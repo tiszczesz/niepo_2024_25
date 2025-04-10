dd5e0eb8534f8b908ec5621a45bd914e46eff970
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-04-10 14:41:19 +0200

    jj

--- 4tip/mobileApp/cw1/app/src/main/java/com/example/cw1/MainActivity.kt
+++ 4tip/mobileApp/cw1/app/src/main/java/com/example/cw1/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.cw1
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +13,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,8 +24,28 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val btn1 = findViewById<Button>(R.id.btn1)
+        val image = findViewById<ImageView>(R.id.iv1)
+        val etDivider = findViewById<EditText>(R.id.etDivider)
+        val btnCounter = findViewById<Button>(R.id.btnCounter)
+        val tvCounter = findViewById<TextView>(R.id.tvCounter)
+        tvCounter.text = counter.toString() //zero na poczatek
+        btnCounter.setOnClickListener {
+            counter++;
+            tvCounter.text = counter.toString()
+            val div = etDivider.text.toString().toInt()
+            changeVisibility(image, div)
+        }
         btn1.setOnClickListener {
-            Toast.makeText(this,"Przycisk zadziałał",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Przycisk zadziałał", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun changeVisibility(image: ImageView, div: Int = 2) {
+
+        if (counter % div == 0) {
+            image.visibility = ImageView.VISIBLE
+        } else {
+            image.visibility = ImageView.INVISIBLE
         }
     }
 }
