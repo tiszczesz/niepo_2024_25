46482fa3d9b583c0f43fe4c8277d46cbb87179b2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-04-30 09:23:32 +0200

    kkk

---
+++ 2tip/2tip_cpp/cw23_ copy/Examples.cpp
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <string>
+#include <iostream>
+struct First {
+//public:
+	int a;
+	std::string text;
+
+	First() :a(0),text("Hello"){
+		/*a = 0;
+		text = "Hello";*/
+	}
+	First(int a, std::string text) :a(a),text(text){
+		/*this->a = a;
+		this->text = text;*/
+	}
+	void Display() {
+		std::cout << "a = " << a << std::endl;
+		std::cout << "text = " << text << std::endl;
+	}
+};
+
+struct Second {
+	int* pa;
+	std::string text;
+	Second() :pa(new int(0)), text("Hello") {
+		/*pa = new int(0);
+		text = "Hello";*/
+	}
+	Second(int a, std::string text) :pa(new int(a)), text(text) {
+		/*this->pa = new int(a);
+		this->text = text;*/
+	}
+	//konstruktor kopiujacy wbudowany płytkie kopiowanie
+//tutaj kopiowanie tylko adresu pa jest jedno miejsce na 12
+	//Second(const Second& obj) :pa(obj.pa), text(obj.text) {
+	//	this->pa = obj.pa;
+	//	this->text = obj.text;*/
+	//}
+	//utowrzenie nowego pa do ktorego przypisujemy wartosc z obj
+//konstruktor kopiujacy napisany przez nas głebokie kopiowanie
+	Second(const Second& obj) :pa(new int(*obj.pa)), text(obj.text) {
+		/*this->pa = new int(*obj.pa);
+		this->text = obj.text;*/
+	}
+	void Display() {
+		std::cout << "a = " << *pa << std::endl;
+		std::cout << "text = " << text << std::endl;
+	}
+	/*~Second() {
+		delete pa;
+	}*/
+};
---
+++ 2tip/2tip_cpp/cw23_ copy/Source.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include "Examples.cpp"
+
+using namespace std;
+int main() {
+	First obj1(12,"gggg");
+	obj1.Display();
+	First obj2 = obj1;
+	obj2.Display();
+	cout << "========================\n";
+	obj1.a = 100;
+	obj1.Display();
+	obj2.Display();
+	cout << "========================\n";
+	Second obj3(12, "gggg");
+	Second obj4 = obj3;
+	obj3.Display();
+	obj4.Display();
+	cout << "========================\n";
+	*obj3.pa = 100;
+	obj3.Display();
+	obj4.Display();
+	return 0;
+}
