c8c2eab11b3b1923a95ef3ddcebbc327981a5c73
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-05-28 14:38:11 +0200

    hh

---
+++ 2tip/2tip_web/cw11_VS2022/Controllers/NonameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cw11_VS2022.Controllers
+{
+    public class NonameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 2tip/2tip_web/cw11_VS2022/Program.cs
@@ -0,0 +1,12 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseStaticFiles();
+//app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Noname}/{action=Index}/{id?}"
+    );
+    
+app.Run();
