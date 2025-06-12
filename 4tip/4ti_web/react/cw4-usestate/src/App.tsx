09684867d845644e561f75917b1838bb1812ac79
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-06-12 09:54:13 +0200

    kkk

--- 4tip/4ti_web/react/cw4-usestate/src/App.tsx
+++ 4tip/4ti_web/react/cw4-usestate/src/App.tsx
@@ -5,10 +5,10 @@ import { useState, type ChangeEvent } from 'react';
 
 function App() {
   console.log('App renderuje się');
-  
+
   // Stan komponentu
-  const [text,setText] = useState('stan początkowy122');
-  const [number1,setNumber1] = useState('0');
+  const [text, setText] = useState('stan początkowy122');
+  const [number1, setNumber1] = useState('0');
   function handleNumber(e: ChangeEvent<HTMLInputElement>): void {
     //tutuj możemy zmienić stan komponentu
     //walidacja danych wejściowych
@@ -21,12 +21,13 @@ function App() {
   // setText("ffffff"); // To jest poprawne, ponieważ używa funkcji do aktualizacji stanu
   return (
     <div className='container'>
-     <h1>Praca ze stanem komponentu</h1>
-     <input onChange={(e)=>{setText(e.target.value)}} type="text"className='form-control' value={text} />
-    {text.toUpperCase()}
-     <input onChange={(e)=>handleNumber(e)} 
-     type="number"className='form-control' value={number1} />
-    {number1}
+      <h1>Praca ze stanem komponentu</h1>
+      <input onChange={(e) => { setText(e.target.value) }} type="text" className='form-control'
+        value={text} />
+      {text.toUpperCase()}
+      <input onChange={(e) => handleNumber(e)}
+        type="number" className='form-control' value={number1} />
+      {number1}
     </div>
   )
 }
